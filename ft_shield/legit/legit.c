6137952a2d24e667d5ad889447bca29f959afea8
IvanT <ivan.teohjh@gmail.com>
2026-06-03 11:17:02 +0800

    legit actually prints user now

--- ft_shield/legit/legit.c
+++ ft_shield/legit/legit.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
+#include <pwd.h>
+#include <unistd.h>
 #include "legit.h"
 
 void run_legit(unsigned char *input, int size)
 {
 	(void) size;
-	printf("jng\n");
+	struct passwd *pw = getpwuid(getuid());
+	printf("%s\n", pw->pw_name);
 
 	for (size_t i = 0; i < 10; i++)
 	{
